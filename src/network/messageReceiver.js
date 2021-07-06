5952ba1e2c4ddae055f459bbffefabec3c0bd4b3
DemonExposer <kbdmns@gmail.com>
2021-07-06 16:49:22 +0200

    Added a cooldown of 1001ms between messages

--- src/network/messageReceiver.js
+++ src/network/messageReceiver.js
@@ -7,6 +7,7 @@ const Net = require("net");
 class MessageReceiver {
 	#socketServer;
 	#boundChannels;
+	#buffer = "";
 
 	/**
 	 * Opens a new server socket on a specified port
@@ -18,24 +19,38 @@ class MessageReceiver {
 
 		this.#socketServer.listen(port, () => console.log(`Listening for messages on localhost:${port}`));
 		this.#socketServer.on("connection", this.#onConnection.bind(this));
+
+		setInterval(this.#messageSender.bind(this), 1001); // Makes sure the time between two messages sent is > 1s
 	}
 
 	/**
-	 * Gets called when a message is received on the server socket and forwards said message
-	 * to Discord
+	 * Sends the message in the buffer to Discord and clears the buffer
+	 */
+	#messageSender() {
+		if (this.#buffer === "")
+			return;
+
+		var tempBuffer = this.#buffer;
+		this.#buffer = "";
+		this.#boundChannels.forEach(channel => channel.send(tempBuffer.slice(0, tempBuffer.length-1)));
+	}
+
+	/**
+	 * Gets called when a message is received on the server socket and adds said message
+	 * to the buffer
 	 * @param {Net.Socket} socket 
 	 */
 	#onConnection(socket) {
-		socket.on("data", chunk => this.#boundChannels.forEach(channel => {
+		socket.on("data", chunk => {
 			try {
 				var jsonObj = JSON.parse(chunk.toString()); // A message will never be larger than a chunk can handle so this should be fine
-				var msg = (jsonObj["sender"] === undefined) ? jsonObj["message"] : `**${jsonObj["sender"]}:** ${jsonObj["message"]}`
-				channel.send(msg);
+				var msg = (jsonObj["sender"] === undefined) ? jsonObj["message"] : `**${jsonObj["sender"]}:** ${jsonObj["message"]}`;
+				this.#buffer += `${msg}\n`;
 			} catch {
 				console.log(`Invalid JSON: ${chunk.toString()}`);
-				channel.send("Message data lost");
+				this.#buffer += "Message data lost\n";
 			}
-		}));
+		});
 	}
 
 	/**
